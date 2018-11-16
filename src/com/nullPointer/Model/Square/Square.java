0d90723389c0744f10de70b3010b2e916a793b08
Berkay Barlas <fpberkay@gmail.com>
2018-11-17 01:33:27 +0300

    :(((

--- src/com/nullPointer/Model/Square/Square.java
+++ src/com/nullPointer/Model/Square/Square.java
@@ -4,18 +4,21 @@ import com.nullPointer.Model.GameEngine;
 import com.nullPointer.Model.Player;
 
 public abstract class Square {
-	private String name;
-	private String type;
-	
-	public Square(String n,String t){
-		name=n;
-		type=t;
-	}
-	public String getName(){
-		return name;
-	}
-	public String getType(){
-		return type;
-	}
-	public abstract void evaluateSquare(GameEngine gameEngine);
+    private String name;
+    private String type;
+
+    public Square(String name, String type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public abstract void evaluateSquare(GameEngine gameEngine);
 }
