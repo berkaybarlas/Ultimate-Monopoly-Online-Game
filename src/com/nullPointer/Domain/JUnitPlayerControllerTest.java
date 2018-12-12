80bdbd8331670a1cafe669977228f9d0a462b86a
Furkan Sahbaz <fsahbaz16@ku.edu.tr>
2018-12-12 22:33:24 +0300

    Generated JUnit Test for Player Controller.

---
+++ src/com/nullPointer/Domain/JUnitPlayerControllerTest.java
@@ -0,0 +1,26 @@
+package com.nullPointer.Domain;
+
+import static org.junit.Assert.*;
+
+public class JUnitPlayerControllerTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.Test
+    public void nextPlayer() {
+    }
+
+    @org.junit.Test
+    public void putInJail() {
+    }
+
+    @org.junit.Test
+    public void getOutFromJail() {
+    }
+
+    @org.junit.Test
+    public void movePlayer() {
+    }
+}
