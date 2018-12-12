7cf7c2332925e56d421357e69d04fd928a5f5b88
Furkan Sahbaz <fsahbaz16@ku.edu.tr>
2018-12-12 22:34:14 +0300

    Generated JUnit Test for the Property Square class.

---
+++ src/com/nullPointer/Domain/JUnitPropertySquareTest.java
@@ -0,0 +1,14 @@
+package com.nullPointer.Domain;
+
+import static org.junit.Assert.*;
+
+public class JUnitPropertySquareTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.Test
+    public void isOwned() {
+    }
+}
