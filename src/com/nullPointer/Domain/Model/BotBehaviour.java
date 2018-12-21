05bb7fd0218630ae3077048616b097fa78bb28f9
Baran Berkay Hökelek <bhokelek16@ku.edu.tr>
2018-12-21 17:16:13 +0300

    added bot timer

--- src/com/nullPointer/Domain/Model/BotBehaviour.java
+++ src/com/nullPointer/Domain/Model/BotBehaviour.java
@@ -3,6 +3,9 @@ package com.nullPointer.Domain.Model;
 import com.nullPointer.Domain.Controller.CommunicationController;
 import com.nullPointer.Domain.Observer;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class BotBehaviour implements Observer {
 
     private GameEngine gg = GameEngine.getInstance();
@@ -16,24 +19,42 @@ public class BotBehaviour implements Observer {
     // Lazy bot
     @Override
     public void onEvent(String message) {
-        System.out.println("Bot is thinking...");
+
+        System.out.println("Incoming message: " + message);
 
         if(gg.amIBot() && gg.isMyTurn())
         {
-            if(message.equals("rollDice"))
-            {
-                if(gg.getRoll3())  gg.roll3Dice();
-                else gg.rollDice();
-                cc.sendClientMessage("dice/" + gg.getLastDiceValues());
-            }
-            else if (message.equals("drawCard"))
-            {
-                cc.sendClientMessage("card/draw");
-            }
-            else
-            {
-                cc.sendClientMessage("next");
-            }
+            Timer timer = new Timer();
+
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    System.out.println("Bot is thinking...");
+                    if(message.equals("rollDice"))
+                    {
+                        if(gg.getRoll3())  gg.roll3Dice();
+                        else gg.rollDice();
+                        System.out.println("The Bot has decided to Roll the Dice!");
+                        cc.sendClientMessage("dice/" + gg.getLastDiceValues());
+                    }
+                    else if (message.equals("drawCard"))
+                    {
+                        System.out.println("The Bot has decided to Draw a Card!");
+                        cc.sendClientMessage("card/draw");
+
+                    }
+//            else if(message.equals("game/start") || message.contains("create") ||message.contains("client") || message.contains("message") || message.contains("newPlayer") || message.contains())
+//            {
+//
+//            }
+                    else if (message.contains("buy") || message.contains("paid rent"))
+                    {
+                        System.out.println("The Bot has decided to Yield its Turn!");
+                        cc.sendClientMessage("player/next");
+                    }
+                }
+            }, 3*1000);
+
         }
 
     }
