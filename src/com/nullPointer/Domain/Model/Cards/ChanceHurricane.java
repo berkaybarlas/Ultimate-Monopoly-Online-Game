4f14e7a5f8a5f5303d90af69e3c54c31192ab678
tumay <tumayozdemir96@gmail.com>
2019-01-04 13:08:39 +0300

    completed improve property

--- src/com/nullPointer/Domain/Model/Cards/ChanceHurricane.java
+++ src/com/nullPointer/Domain/Model/Cards/ChanceHurricane.java
@@ -1,6 +1,8 @@
 package com.nullPointer.Domain.Model.Cards;
 
 import com.nullPointer.Domain.Model.GameEngine;
+import com.nullPointer.Domain.Model.Square.PropertySquare;
+import com.nullPointer.Domain.Model.Square.Square;
 
 public class ChanceHurricane extends ChanceCard {
 
@@ -19,11 +21,16 @@ public class ChanceHurricane extends ChanceCard {
 				e.printStackTrace();
 			}
 		}
-		System.out.println("[Hurricane Card] Square: "+gameEngine.getDomainBoard().getSquareAt(gameEngine.getChosenSquareIndex()));
+		System.out.println("[Hurricane Card] Chosen Square: "+gameEngine.getDomainBoard().getSquareAt(gameEngine.getChosenSquareIndex()));
 		System.out.println("[Hurricane Card] Remove 1 House from each property in any player’s 1 color group. (Downgrade Skyscrapers to Hotels; Hotels to 4 houses.) ");
 		//Remove 1 House from each property in any player’s 1 color group. (Downgrade Skyscrapers to Hotels; Hotels to 4 houses.) 
 		//in the end of the turn
-		
+		Square currentSquare = gameEngine.getDomainBoard().getSquareAt(gameEngine.getChosenSquareIndex());		
+		PropertySquare pS= ((PropertySquare) currentSquare);
+		if(pS.isOwned()){
+			if(pS.numHouses()>0 || pS.hasHotel() || pS.hasSkyscraper())
+				pS.downgrade();
+		}
 		gameEngine.setChosenSquareIndex(-1);
 		
 	}
